2a2a1de8a7b5e51dcab2ef8c08cbbf86da317715
BhawickJain <53153810+BhawickJain@users.noreply.github.com>
2022-09-11 18:30:10 +0000

    Deploying to gh-pages from @ BhawickJain/grid-spa@68fdccb8f0e03e335733b3595ee911d9c1c91dcf 🚀

---
+++ _next/static/d15R8sr6bUSDSBA9Nh1G3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f6667b54ce623550.css","static/chunks/pages/index-086a7f3c0e4d8d6a.js"],"/404":["static/chunks/pages/404-6aae83fa57711cd2.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
